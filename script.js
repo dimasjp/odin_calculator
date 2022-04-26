cf1d0122586ff575b9bebc6478bceb77d0f8e663
dimsjp.win <diims.el@gmail.com>
2022-04-26 15:31:44 +0700

    Add function operate

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case 'x':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+    }
+}
