4042a2a0480c099172ff7e518e4653907b3c111e
dimsjp.win <diims.el@gmail.com>
2022-05-11 22:05:38 +0700

    Add janky basic functions to calculate

--- script.js
+++ script.js
@@ -2,27 +2,86 @@ const numberButton = document.querySelectorAll('.number');
 const operatorButton = document.querySelectorAll('.operator');
 const equalButton = document.querySelector('#equal');
 const decimalButton = document.querySelector('#decimal');
+const clearButton = document.querySelector('#clear');
 const inputScreen = document.querySelector('#input');
-const lastOperationScreen = document.querySelector('#lastOperation');
+
+let currentNum = '';
+let storedNum = '';
+let currentOperation = '';
 
 
 numberButton.forEach((button) =>
-    button.addEventListener('click', () => insertNumber(button.textContent))
+    button.addEventListener('click', () => logNumber(button.textContent))
 )
 
-function insertNumber(number) {
-    if (inputScreen.textContent === '0')
-        resetScreen()
-    inputScreen.textContent += number
+operatorButton.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+clearButton.addEventListener('click', clear);
+equalButton.addEventListener('click', evaluate);
+
+function logNumber(number) {
+    if (currentNum === '0') {
+        currentNum = '';
+    }
+    currentNum += number;
+    updateScreen(currentNum);
 }
 
-function resetScreen() {
-    inputScreen.textContent = ''
+function updateScreen(input) {
+    if (input === 'Infinity') {
+        inputScreen.textContent = "ERROR";
+    } else {
+        inputScreen.textContent = input;
+    }
+}
+
+function clear() {
+    currentNum = '0';
+    storedNum = '';
+    currentOperation = '';
+    updateScreen(currentNum);
+}
+
+function setOperation(operator) {
+    if (currentOperation != '') evaluate();
+    if (!storedNum) {
+        storedNum = currentNum;
+        currentOperation = operator;
+        currentNum = '';
+    } else if (storedNum && !currentOperation) {
+        currentOperation = operator;
+        updateScreen(storedNum);
+        currentNum = '';
+    } else if (!currentNum && currentOperation) {
+        currentOperation = operator;
+    } else {
+        storedNum = operate(currentOperation, storedNum, currentNum);
+        updateScreen(storedNum);
+        currentNum = '';
+        currentOperation = operator
+    }
+}
+
+function evaluate() {
+    if (currentOperation === '') return;
+    if (currentOperation === '/' && currentNum === '0') {
+        alert("Divide by 0!")
+        return
+    };
+    inputScreen.textContent = roundResult(
+        operate(currentOperation, storedNum, currentNum)
+    )
+    currentOperation = '';
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
 }
 
 
-
-//MATHS
+//MATHS!🤓
 function add(a, b) {
     return a + b;
 }
@@ -39,16 +98,19 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    if (operator == "+") {
-        return add(a, b)
-    } else if (operator == "-") {
-        return subtract(a, b)
-    } else if (operator == "*") {
-        return multiply(a, b)
-    } else if (operator == "/") {
-        return divide(a, b)
-    } else {
-        
+function operate(operator, a, b) {    
+    a = Number(a);
+    b = Number(b);
+    switch(operator) {
+        case "+":
+            return add(a, b);
+         case "-":
+            return subtract(a, b)
+         case "*":
+            return multiply(a, b)
+         case "/":
+            return divide(a, b)
+         default:
+            return null
     }
 }
